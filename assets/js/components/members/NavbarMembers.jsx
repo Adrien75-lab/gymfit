04bebe05709216eb9a5dc37d32bf0574e859d5ab
Adrien75-lab <adrichristophe@gmail.com>
2023-05-14 14:01:46 +0200

    fix de la navbar

--- assets/js/components/members/NavbarMembers.jsx
+++ assets/js/components/members/NavbarMembers.jsx
@@ -1,59 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavbarMembers = ({ linkToCustomers, linkText, tokenPayload,linkText2 }) => {
+const NavbarMembers = ({ linkToCustomers, linkText, tokenPayload, linkText2 }) => {
   return (
     <div className="container w-50" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
 
-        <nav class="container-fluid">
-          <div class="row">
-            <div class="col-md-3">
-              <Link
-                to="/programmerSeance"
-                className="btn btn-primary active"
-                role="button"
-                data-bs-toggle="button"
-                aria-pressed="true"
-              >
-                Rechercher sa salle
-              </Link>
-            </div>
-            <div class="col-md-3">
-              <Link
-                to={linkToCustomers}
-                class="btn btn-primary"
-                aria-disabled="true"
-                role="button"
-                data-bs-toggle="button"
-              >
-                {linkText2}
-              </Link>
-            </div>
-            <div class="col-md-3">
-              <Link
-                to={linkToCustomers}
-                class="btn btn-primary"
-                aria-disabled="true"
-                role="button"
-                data-bs-toggle="button"
-              >
-                {linkText}
-              </Link>
-            </div>
-            <div class="col-md-3">
-              <Link
-                to={`/listbooking/${tokenPayload.Id}`}
-                class="btn btn-primary"
-                aria-disabled="true"
-                role="button"
-                data-bs-toggle="button"
-              >
-                 Voir ses reservations
-              </Link>
-            </div>
+      <nav class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+            <Link
+              to="/programmerSeance"
+              className="btn btn-primary btn-fixed-width"
+              role="button"
+              data-bs-toggle="button"
+            >
+              Rechercher sa salle
+            </Link>
           </div>
-        </nav>
-      </div>
+          <div class="col-md-3">
+            <Link
+              to={`/listbooking/${tokenPayload.Id}`}
+              className="btn btn-primary btn-fixed-width"
+              role="button"
+              data-bs-toggle="button"
+            >
+              Voir ses rdv
+            </Link>
+          </div>
+          <div class="col-md-3">
+            <Link
+              to={linkToCustomers}
+              className="btn btn-primary btn-fixed-width"
+              role="button"
+              data-bs-toggle="button"
+            >
+              {linkText}                 
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
