5f51916f40f79b72cac285ce3bf86d643e3089e3
Adrien75-lab <adrichristophe@gmail.com>
2023-05-14 15:35:15 +0200

    fix de la navbar

--- assets/js/components/members/NavbarMembers.jsx
+++ assets/js/components/members/NavbarMembers.jsx
@@ -4,39 +4,33 @@ import { Link } from 'react-router-dom';
 const NavbarMembers = ({ linkToCustomers, linkText, tokenPayload, linkText2 }) => {
   return (
     <div className="container w-50" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-
+  
       <nav class="container-fluid">
-        <div class="row">
-          <div class="col-md-3">
-            <Link
-              to="/programmerSeance"
-              className="btn btn-primary btn-fixed-width"
-              role="button"
-              data-bs-toggle="button"
-            >
-              Rechercher sa salle
-            </Link>
-          </div>
-          <div class="col-md-3">
-            <Link
-              to={`/listbooking/${tokenPayload.Id}`}
-              className="btn btn-primary btn-fixed-width"
-              role="button"
-              data-bs-toggle="button"
-            >
-              Voir ses rdv
-            </Link>
-          </div>
-          <div class="col-md-3">
-            <Link
-              to={linkToCustomers}
-              className="btn btn-primary btn-fixed-width"
-              role="button"
-              data-bs-toggle="button"
-            >
-              {linkText}                 
-            </Link>
-          </div>
+        <div class="row" style={{ justifyContent: "space-between" }}>
+          <Link
+            to="/programmerSeance"
+            className="btn btn-primary btn-fixed-width"
+            role="button"
+            data-bs-toggle="button"
+          >
+            Rechercher sa salle
+          </Link>
+          <Link
+            to={`/listbooking/${tokenPayload.Id}`}
+            className="btn btn-primary btn-fixed-width"
+            role="button"
+            data-bs-toggle="button"
+          >
+            Voir ses rdv
+          </Link>
+          <Link
+            to={linkToCustomers}
+            className="btn btn-primary btn-fixed-width"
+            role="button"
+            data-bs-toggle="button"
+          >
+            {linkText}                 
+          </Link>
         </div>
       </nav>
     </div>
